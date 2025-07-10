3cbe4f59c7fb3ac06adf402a883a9a8910de7eff
krish kumar <krishchahit1605@gmail.com>
2025-07-10 20:31:15 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    transpilePackages: ['supports-color'],
+    experimental: {
+      esmExternals: 'loose',
+    },
+    reactStrictMode: true,
+  }
+  
+  module.exports = nextConfig;
+  
