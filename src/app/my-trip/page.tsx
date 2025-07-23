e1ac25f935d53e2f505f5f7d985f41b512986f57
krish kumar <krishchahit1605@gmail.com>
2025-07-23 21:42:53 +0530

    show

--- src/app/my-trip/page.tsx
+++ src/app/my-trip/page.tsx
@@ -74,30 +74,31 @@ export default async function ViewTripPage() {
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     {tripsWithImages.map((trip) => (
-                        <Link
-                            key={trip.id}
-                            href={`/view-trip/${trip.id}`}
-                            className="rounded-xl overflow-hidden shadow-md hover:shadow-xl cursor-pointer transition border border-orange-400 h-[320px] flex flex-col"
-                        >
-                            <div className="relative h-[80%] w-full">
-                                <Image
-                                    src={trip.image}
-                                    alt={trip.destination}
-                                    fill
-                                    className="object-cover"
-                                />
-                            </div>
-                            <div className="h-[20%] px-4 py-2 bg-white/75 rounded-md">
-                                <h2 className="text-xl font-bold text-orange-700 truncate">
-                                    {trip.destination}
-                                </h2>
-                                <p className="text-sm text-black">
-                                    {trip.days} Days trip with {trip.budget} Budget
-                                </p>
-                                <TripRatingStars tripId={trip.id} initialRating={trip.rating ?? undefined} />
-                            </div>
-                        </Link>
-                        
+                        <div>
+                            <Link
+                                key={trip.id}
+                                href={`/view-trip/${trip.id}`}
+                                className="rounded-xl overflow-hidden shadow-md hover:shadow-xl cursor-pointer transition border border-orange-400 h-[320px] flex flex-col"
+                            >
+                                <div className="relative h-[80%] w-full">
+                                    <Image
+                                        src={trip.image}
+                                        alt={trip.destination}
+                                        fill
+                                        className="object-cover"
+                                    />
+                                </div>
+                                <div className="h-[20%] px-4 py-2 bg-white/75 rounded-md">
+                                    <h2 className="text-xl font-bold text-orange-700 truncate">
+                                        {trip.destination}
+                                    </h2>
+                                    <p className="text-sm text-black">
+                                        {trip.days} Days trip with {trip.budget} Budget
+                                    </p>
+                                </div>
+                            </Link>
+                            <TripRatingStars tripId={trip.id} initialRating={trip.rating ?? undefined} />
+                        </div>
                     ))}
 
                 </div>
